9e23b8dea4ea7c197e7fffa4c5a05010b0ccaab4
Skyler Moore-Firkins <skyler.moore-firkins@certilytics.com>
2018-03-08 13:39:00 -0500

    initial commit

---
+++ src/main/scala/testdomain/Test.scala
@@ -0,0 +1,33 @@
+package testdomain
+
+import org.neo4j.ogm.session.{Session, SessionFactory}
+import org.neo4j.ogm.config.{Configuration, ConfigurationSource, ClasspathConfigurationSource}
+import org.neo4j.ogm.annotation.{NodeEntity, Relationship, Id, GeneratedValue, GraphId, Property}
+
+@NodeEntity
+class Person {
+  @GraphId
+  @Property
+  var id: java.lang.Long = _
+
+  @Property
+  var firstName: String = _
+
+  @Property
+  var lastName: String = _
+
+  def this(firstName: String, lastName: String) {
+    this()
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
+object Test extends App {
+  val configuration = new Configuration.Builder(new ClasspathConfigurationSource("ogm.properties")).build()
+  val sessionFactory = new SessionFactory(configuration, "com.certilytics.mapper", "com.certilytics.mapper.NeoModels")
+  val session = sessionFactory.openSession()
+
+  val me = new Person("The", "Wizard")
+  session.save(me)
+}
